10110d77e4b3f5fde2436fb28d2e0f24bbbc63a1
Cole Brazinski <colebrazinski15@gmail.com>
2026-09-25 14:58:03 -0400

    fix: update frontend to use new api routes

--- frontend/src/api/status.ts
+++ frontend/src/api/status.ts
@@ -5,7 +5,7 @@ export interface ScoresResponse extends PolledAPIResponse {
 };
 
 export async function getScores(): Promise<ScoresResponse> {
-    const res = await fetch("/api/status/get_scores");
+    const res = await fetch("/api/status/scores");
     return res.json();
 }
 
@@ -14,17 +14,22 @@ export interface RoundHistoryResponse extends PolledAPIResponse {
 }
 
 export async function getRoundHistory(): Promise<RoundHistoryResponse> {
-    const res = await fetch("/api/status/get_round_history");
+    const res = await fetch("/api/status/round_history");
     return res.json();
 }
 
 export async function getRoundHistoryCumulative(): Promise<RoundHistoryResponse> {
-    const res = await fetch("/api/status/get_cumulative_round_history");
+    const res = await fetch("/api/status/cumulative_round_history");
     return res.json();
 }
 
 export async function getOverview(): Promise<OverviewResponse> {
-    const res = await fetch("/api/status/get_overview");
+    const res = await fetch("/api/status/overview");
+    return res.json();
+}
+
+export async function getInformation(): Promise<OverviewResponse> {
+    const res = await fetch("/api/status/info");
     return res.json();
 }
 
@@ -40,5 +45,12 @@ interface ServiceStatus {
 
 export interface OverviewResponse extends PolledAPIResponse {
     overview: Record<string, TeamOverview>
+}
+
+export interface InfoResponse extends PolledAPIResponse {
+    event_name_long: string,
+    event_name_short: string,
+    organization_name_long: string,
+    organization_name_short: string
 }
 
